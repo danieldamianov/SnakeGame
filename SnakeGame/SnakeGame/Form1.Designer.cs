2e6cba66d2c5a1a126ea242b95e18d21c3fd8398
danieldamianov <danieldamianov02@gmail.com>
2019-11-12 00:20:27 +0200

    Added functionality for self - eating

--- SnakeGame/SnakeGame/Form1.Designer.cs
+++ SnakeGame/SnakeGame/Form1.Designer.cs
@@ -30,17 +30,26 @@
         {
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // timer1
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 0;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(982, 953);
+            this.ClientSize = new System.Drawing.Size(664, 654);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -51,6 +60,7 @@
         #endregion
 
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- SnakeGame/SnakeGame/Form1.cs
+++ SnakeGame/SnakeGame/Form1.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -29,12 +30,16 @@ namespace SnakeGame
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.Size = new Size(500, 500);
+            
+            this.ClientSize = new Size(800, 800);
             this.food = new Button();
+            this.panel1.ClientSize = new Size(500, 500);
+            this.panel1.BackColor = Color.Red;
             this.food.Size = new Size(GlobalConstraints.SnakeWidth,GlobalConstraints.SnakeWidth);
-            this.food.Location = new Point(new Random().Next(0, GlobalConstraints.FormSize - GlobalConstraints.SnakeWidth),
-                new Random().Next(0, GlobalConstraints.FormSize - GlobalConstraints.SnakeWidth));
-            this.Controls.Add(this.food);
+            this.food.Location = new Point(new Random().Next(50, GlobalConstraints.FormSize - GlobalConstraints.SnakeWidth - 50),
+                new Random().Next(50, GlobalConstraints.FormSize - GlobalConstraints.SnakeWidth - 50));
+            this.food.Location = new Point(470, 470);
+            this.panel1.Controls.Add(this.food);
             this.snake = new Snake();
             this.direction = Direction.Right;
             this.snake.snakePartsLocations[1].direction = Enums.Direction.Right;
@@ -47,7 +52,8 @@ namespace SnakeGame
             this.snakeButtons[1].Size = new Size(GlobalConstraints.SnakeWidth,GlobalConstraints.SnakeWidth);
             this.snakeButtons[1].Location = new Point(GlobalConstraints.SnakeWidth, 0);
             this.snakeButtons[1].Enabled = false;
-            this.Controls.AddRange(this.snakeButtons.ToArray());
+            this.snakeButtons[1].BackColor = Color.Black;
+            this.panel1.Controls.AddRange(this.snakeButtons.ToArray());
             this.timer1.Interval = 100;
             this.timer1.Start();
         }
@@ -91,29 +97,49 @@ namespace SnakeGame
         private void timer1_Tick(object sender, EventArgs e)
         {
             MoveSnake();
+            CheckForEatingItself();
+        }
+
+        private void CheckForEatingItself()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var head = this.snakeButtons.Last();
+            File.AppendAllText("log.txt", stopwatch.Elapsed.ToString());
+            stopwatch.Start();
+            var body = this.snakeButtons.Take(this.snakeButtons.Count - 1);
+            File.AppendAllText("log.txt", stopwatch.Elapsed.ToString());
+            stopwatch.Start();
+            if (body.Any(p => this.TwoButtonsOverlap(p, head)))
+            {
+                File.AppendAllText("log.txt", stopwatch.Elapsed.ToString());
+                MessageBox.Show("You lost!");
+                File.AppendAllText("log.txt", Environment.NewLine);
+            }
         }
 
         private bool TwoButtonsOverlap(Button button1,Button button2)
         {
-            for (int i = button1.Location.X; i <= button1.Location.X + GlobalConstraints.SnakeWidth - 1; i++)
-            {
-                for (int j = button1.Location.Y; j <= button1.Location.Y + GlobalConstraints.SnakeWidth - 1; j++)
-                {
-                    for (int k = button2.Location.X; k <= button2.Location.X + GlobalConstraints.SnakeWidth - 1; k++)
-                    {
-                        for (int m = button2.Location.Y; m <= button2.Location.Y + GlobalConstraints.SnakeWidth - 1; m++)
-                        {
-                            if (i == k && j == m)
-                            {
-                                return true;
-                            }
-                        }
-                    }
-                }
-            }
-            return false;
+            return DoOverlap(new Point(button1.Location.X, button1.Location.Y), new Point(button1.Location.X + 30, button1.Location.Y + 30),
+                new Point(button2.Location.X, button2.Location.Y), new Point(button2.Location.X + 30, button2.Location.Y + 30));
         }
+        private bool DoOverlap(Point l1, Point r1,
+                          Point l2, Point r2)
+        {
+            // If one rectangle is on left side of other  
+            if (l1.X >= r2.X || l2.X >= r1.X)
+            {
+                return false;
+            }
 
+            // If one rectangle is above other 
+            if (l1.Y >= r2.Y || l2.Y >= r1.Y) return false;
+            //if (l1.Y < r2.Y || l2.Y < r1.Y)
+            //{
+            //    return false;
+            //}
+            return true;
+        }
         int counter = 0;
         private void MoveSnake()
         {
@@ -125,12 +151,12 @@ namespace SnakeGame
 
                 this.snakeButtons.Insert(0, new Button());
                 this.snakeButtons[0].Size = new Size(GlobalConstraints.SnakeWidth, GlobalConstraints.SnakeWidth);
-                this.Controls.Add(snakeButtons[0]);
+                this.panel1.Controls.Add(snakeButtons[0]);
                 //this.snakeButtons[0].Location = new Point(0, 0);
                 this.snakeButtons[0].Enabled = false;
                 Random random = new Random();
-                this.food.Location = new Point(random.Next(0, GlobalConstraints.FormSize - GlobalConstraints.SnakeWidth),
-                random.Next(0, GlobalConstraints.FormSize - GlobalConstraints.SnakeWidth));
+                this.food.Location = new Point(random.Next(50, GlobalConstraints.FormSize - GlobalConstraints.SnakeWidth - 50),
+                random.Next(50, GlobalConstraints.FormSize - GlobalConstraints.SnakeWidth - 50));
                 File.AppendAllText("logs.txt", $"{this.food.Location.X} {this.food.Location.Y}{Environment.NewLine}");
             }
             
